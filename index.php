d05a61071a5a36a3f697706bf6abbc7bc2c71b08
David <dp_play.music@hotmail.com>
2016-01-05 19:04:36 -0600

    Create repo

---
+++ index.php
@@ -0,0 +1,50 @@
+<!--
+|
+| View Design -> Index -> Login
+|
+-->
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <!-- Meta -->
+        <meta charset="utf-8"/>
+        <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+        <meta name="authors" content="dp-DevelopmentSoftware"/>
+        <meta name="keywords" content="Daesy Hinojosa" />
+        <meta name="description" content="Diputada Daesy Hinojosa" />
+        <meta name="robots" content="all" />      
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <title>Diputada Daesy Hinojosa</title>
+        <link href="" rel="icon"/>
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="public/css/bootstrap.css"/>
+        <link rel="stylesheet" href="public/css/main.css"/>
+
+        <!-- JS -->
+        <script src="public/js/jquery-2.1.4.js"></script>
+        <script src="public/js/bootstrap.js"></script>
+    </head>
+    <body>
+        <section id="home" data-type="parallax_section" data-speed="10">
+            <article>
+                <div class="geek"></div>
+                <h2>Pagina #1</h2>
+            </article>
+        </section>
+        <section id="section2" data-type="parallax_section" data-speed="10">
+            <article>
+                <div class="geek"></div>
+                <h2>Pagina #2</h2>
+            </article>
+        </section>
+        <section id="section3" data-type="parallax_section" data-speed="10">
+            <article>
+                <div class="geek"></div>
+                <h2>Pagina #3</h2>
+            </article>
+        </section>
+    </body>
+</html>
